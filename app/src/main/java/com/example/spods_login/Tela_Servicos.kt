e60817c7b8ce7f834e36e2236104f83d1f694897
PaulaAndrade <santos.andradepaula@gmail.com>
2023-04-23 16:10:07 -0300

    Protótipos de Telas (Inicial,Servicos e Clientes)

---
+++ app/src/main/java/com/example/spods_login/Tela_Servicos.kt
@@ -0,0 +1,11 @@
+package com.example.spods_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tela_Servicos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_servicos)
+    }
+}
---
+++ app/src/main/java/com/example/spods_login/solicitacoes.kt
@@ -0,0 +1,11 @@
+package com.example.spods_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class solicitacoes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_solicitacoes)
+    }
+}

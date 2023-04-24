2136f6920338326ef582409987e6476d23a2184c
PaulaAndrade <santos.andradepaula@gmail.com>
2023-04-24 16:56:18 -0300

    Prótotipos corrigidos

---
+++ app/src/main/java/com/example/spods_login/cad_ordem_serv.kt
@@ -0,0 +1,11 @@
+package com.example.spods_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cad_ordem_serv : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cad_ordem_serv)
+    }
+}
---
+++ app/src/main/java/com/example/spods_login/lista_ordens.kt
@@ -0,0 +1,11 @@
+package com.example.spods_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class lista_ordens : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_ordens)
+    }
+}

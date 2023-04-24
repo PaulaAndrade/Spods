3e580645575d6ff38b63623f0c182960257b4831
PaulaAndrade <santos.andradepaula@gmail.com>
2023-04-24 16:06:01 -0300

    Prótotipos

--- app/src/main/java/com/example/spods_login/TelaInicial.kt
+++ app/src/main/java/com/example/spods_login/TelaInicial.kt
@@ -1,8 +1,18 @@
 package com.example.spods_login
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ProgressBar
+import androidx.core.view.isVisible
+import com.example.spods_login.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class TelaInicial : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +22,5 @@ class TelaInicial : AppCompatActivity() {
         supportActionBar!!.hide()
         window.statusBarColor= Color.parseColor("#FCE06D1F")
     }
-
-
 }
+
